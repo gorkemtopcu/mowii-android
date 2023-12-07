06e0ace8ca880c69f508f70d32d8767106bcae86
Can Zunal <98685681+CZunal@users.noreply.github.com>
2023-12-07 21:09:44 +0300

    draft requests

---
+++ mowii/src/main/java/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    public List<User> findByName(String name);
+
+}
