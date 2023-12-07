73f3d307693bbfbd2d483ccea1a17d8bb320840e
Gorkem Topcu <grkmtpcu@gmail.com>
2023-12-07 11:08:41 +0300

    first commit

---
+++ mowii/src/main/java/com/mowii/mowii/MowiiApplication.java
@@ -0,0 +1,13 @@
+package com.mowii.mowii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowiiApplication.class, args);
+	}
+
+}
