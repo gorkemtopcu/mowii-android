b4ada0ff94e4d64c07035c0548f2ab8ae83724c0
Can Zunal <98685681+CZunal@users.noreply.github.com>
2023-12-08 20:28:20 +0300

    Movie adding to collection

--- mowii/src/main/java/com/mowii/mowii/controller/MovieCollectionController.java
+++ mowii/src/main/java/com/mowii/mowii/controller/MovieCollectionController.java
@@ -1,16 +1,16 @@
 package com.mowii.mowii.controller;
 
 import com.mowii.mowii.exception.UserNotFoundException;
-import com.mowii.mowii.model.MovieCollection;
-import com.mowii.mowii.model.MovieCollectionInput;
-import com.mowii.mowii.model.User;
+import com.mowii.mowii.model.*;
 import com.mowii.mowii.service.MovieCollectionService;
+import com.mowii.mowii.service.MovieService;
 import com.mowii.mowii.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,11 +19,13 @@ public class MovieCollectionController {
 
     private final MovieCollectionService movieCollectionService;
     private final UserService userService;
+    private final MovieService movieService;
 
     @Autowired
-    public MovieCollectionController(MovieCollectionService movieCollectionService, UserService userService) {
+    public MovieCollectionController(MovieCollectionService movieCollectionService, UserService userService, MovieService movieService) {
         this.movieCollectionService = movieCollectionService;
         this.userService = userService;
+        this.movieService = movieService;
     }
 
     @GetMapping("/all")
@@ -48,7 +50,36 @@ public class MovieCollectionController {
             return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
         }
     }
-    
+
+    @PutMapping("/add-movie")
+    public ResponseEntity<?> addMovieToCollection(@RequestBody AddMovieToCollectionInput input) {
+        try {
+            Movie movie = movieService.getById(input.getMovieId());
+            MovieCollection movieCollection = movieCollectionService.getById(input.getId());
+
+            if (movieCollection.getMovies() == null) {
+                movieCollection.setMovies(new ArrayList<>()); // Initialize the list if it's null
+            }
+
+            if (movieCollection.getMovies().contains(movie)) {
+                return new ResponseEntity<>("Movie is already in the collection", HttpStatus.BAD_REQUEST);
+            }
+
+            // Add the movie to the collection
+            movieCollection.getMovies().add(movie);
+
+            // Update the movie collection
+            movieCollectionService.updateMovieCollection(movieCollection);
+
+            return new ResponseEntity<>("Movie added to collection successfully", HttpStatus.OK);
+
+        } catch (UserNotFoundException e) {
+            String errorMessage = e.getMessage();
+            return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
     @DeleteMapping("/delete/{id}")
     public void deleteMovieCollection(@PathVariable String id) {
         movieCollectionService.delete(id);
--- mowii/src/main/java/com/mowii/mowii/model/MovieCollection.java
+++ mowii/src/main/java/com/mowii/mowii/model/MovieCollection.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -17,7 +18,7 @@ public class MovieCollection {
     private int like;
 
     @DBRef
-    private List<Movie> movies;
+    private List<Movie> movies = new ArrayList<>();
     @DBRef
     private User user;
 
