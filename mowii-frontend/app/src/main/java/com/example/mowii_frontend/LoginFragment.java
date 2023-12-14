115f35739e3a6e830ad01d64a9042db22c95fa6f
Berk Ay <berk.ay2756@gmail.com>
2023-12-14 15:49:00 +0300

    Updated login fragment

--- mowii-frontend/app/src/main/java/com/example/mowii_frontend/LoginFragment.java
+++ mowii-frontend/app/src/main/java/com/example/mowii_frontend/LoginFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
@@ -60,7 +61,18 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        Button btnLogin = view.findViewById(R.id.btnLogin);
+        String email = ((EditText)view.findViewById(R.id.txtEmail)).getText().toString();
+        String password = ((EditText)view.findViewById(R.id.txtPass)).getText().toString();
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+
+
+
     }
 }
