c7f1f4d7c7e43e1bb6c123a5a67a1920f415104c
Berk Ay <berk.ay2756@gmail.com>
2023-12-14 13:30:03 +0300

    Updated gorkem branch

--- mowii/src/main/java/com/mowii/mowii/exception/MovieCollectionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mowii.mowii.exception;
-
-public class MovieCollectionNotFoundException extends RuntimeException {
-    public MovieCollectionNotFoundException(String message) {
-        super(message);
-    }
-}
