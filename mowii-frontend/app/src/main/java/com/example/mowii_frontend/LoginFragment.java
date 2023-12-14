5034408e0e619a68b1e9606423c8c00f8b6a4b98
Gorkem <grkmtpcu@gmail.com>
2023-12-14 23:58:35 +0300

    Update RegistrationUI

--- mowii-frontend/app/src/main/java/com/example/mowii_frontend/LoginFragment.java
+++ mowii-frontend/app/src/main/java/com/example/mowii_frontend/LoginFragment.java
@@ -7,21 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         TextView switchText = view.findViewById(R.id.txt_switch);
         switchText.setOnClickListener(v -> {
             RegistrationActivity registrationActivity = (RegistrationActivity) requireActivity();
             registrationActivity.selectRegistrationFragment(new SignupFragment());
         });
-        // Add any specific initialization for LoginFragment
+
+        emailEditText = view.findViewById(R.id.etxt_email);
+        passwordEditText = view.findViewById(R.id.etxt_password);
+
         return view;
+    }
+
+    private void onLoginButtonClicked(View view) {
+        String enteredEmail = emailEditText.getText().toString();
+        String enteredPassword = passwordEditText.getText().toString();
+
+        
     }
 }
--- mowii-frontend/app/src/main/java/com/example/mowii_frontend/SignupFragment.java
+++ mowii-frontend/app/src/main/java/com/example/mowii_frontend/SignupFragment.java
@@ -4,22 +4,75 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignupFragment extends Fragment {
+
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        // Switch to Login
         TextView switchText = view.findViewById(R.id.txt_switch);
         switchText.setOnClickListener(v -> {
             RegistrationActivity registrationActivity = (RegistrationActivity) requireActivity();
             registrationActivity.selectRegistrationFragment(new LoginFragment());
         });
-        // Add any specific initialization for LoginFragment
+
+        // Signup button clicked
+        Button signupButton = view.findViewById(R.id.btn_signup);
+        signupButton.setOnClickListener(this::onSignUpButtonClicked);
+
+        usernameEditText = view.findViewById(R.id.etxt_username);
+        emailEditText = view.findViewById(R.id.etxt_email);
+        passwordEditText = view.findViewById(R.id.etxt_password);
+
         return view;
+    }
+
+    private void onSignUpButtonClicked(View view) {
+
+        String enteredUsername = usernameEditText.getText().toString();
+        String enteredEmail = emailEditText.getText().toString();
+        String enteredPassword = passwordEditText.getText().toString();
+
+        if (!isValidUsername(enteredUsername)){
+            Toast.makeText(requireContext(), "Invalid username! Username must be at least 3 characters.", Toast.LENGTH_SHORT).show();
+        }
+        else if (!isValidEmail(enteredEmail)) {
+            Toast.makeText(requireContext(), "Invalid email!", Toast.LENGTH_SHORT).show();
+        }
+        else if (!isValidPassword(enteredPassword)) {
+            Toast.makeText(requireContext(), "Invalid password! Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // TODO: try to register user to database
+            Toast.makeText(requireContext(), "Registration successful", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isValidUsername(String username) {
+        return !TextUtils.isEmpty(username) && username.length() >= 3;
+    }
+
+
+    private boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String password) {
+        return !TextUtils.isEmpty(password) && password.length() >= 6;
     }
 }
