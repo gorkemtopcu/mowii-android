db0108b312a1166ce4bf72a9267455d119591871
Berk Ay <berk.ay2756@gmail.com>
2023-12-14 13:32:35 +0300

    Updated branch

--- mowii/src/main/java/com/mowii/mowii/exception/MovieNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mowii.mowii.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-    public MovieNotFoundException(String message)
-    {
-        super(message);
-    }
-}
